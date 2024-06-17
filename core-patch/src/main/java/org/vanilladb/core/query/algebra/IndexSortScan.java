f280992066d57398b28a0ff32568cfc7446396fc
yuchenweng7@gmail.com <yuchenweng7@gmail.com>
2024-06-17 10:29:03 +0800

    original with SIMD version

--- core-patch/src/main/java/org/vanilladb/core/query/algebra/IndexSortScan.java
+++ core-patch/src/main/java/org/vanilladb/core/query/algebra/IndexSortScan.java
@@ -9,6 +9,7 @@ import org.vanilladb.core.sql.Constant;
 import org.vanilladb.core.sql.VectorConstant;
 import org.vanilladb.core.sql.distfn.DistanceFn;
 import org.vanilladb.core.storage.file.BlockId;
+import org.vanilladb.core.storage.index.IVF.SIMDOperations;
 import org.vanilladb.core.storage.record.RecordId;
 
 public class IndexSortScan implements Scan {
@@ -17,9 +18,9 @@ public class IndexSortScan implements Scan {
     private DistanceFn distFn;
     private Map<Double, Map<Constant, Constant>> distBlkRidMap;
     private Iterator<Entry<Double, Map<Constant, Constant>>> distBlkRidIter;
-    private Map<Double, Constant> distIidMap;
-    private Iterator<Entry<Double, Constant>> distIidIter;
-    private Entry<Double, Constant> cur_entry;
+    private Map<Float, Constant> distIidMap;
+    private Iterator<Entry<Float, Constant>> distIidIter;
+    private Entry<Float, Constant> cur_entry;
 
     public IndexSortScan(Scan s, Scan ds, DistanceFn distFn) {
         this.s = s;
@@ -34,10 +35,13 @@ public class IndexSortScan implements Scan {
 
     @Override
     public void beforeFirst() {
-        distIidMap = new TreeMap<Double, Constant>();
+        distIidMap = new TreeMap<Float, Constant>();
+        float[] queryVector = distFn.getQueryVector().asJavaVal();
         s.beforeFirst();
         while (s.next()) {
-            distIidMap.put(distFn.distance((VectorConstant) s.getVal("i_emb")), s.getVal("i_id"));
+            distIidMap.put((float) SIMDOperations
+                    .simdEuclideanDistance(((VectorConstant) s.getVal("i_emb")).asJavaVal(), queryVector),
+                    s.getVal("i_id"));
         }
         distIidIter = distIidMap.entrySet().iterator();
     }
